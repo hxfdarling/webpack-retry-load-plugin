b9b0c5be5fef9feb11791fc2205fa16377eec4d5
edenhliu <edenhliu@tencent.com>
2018-11-14 15:53:15 +0800

    feat: first blood

---
+++ .commitlintrc.js
@@ -0,0 +1,8 @@
+
+// 校验commit消息是否符合规范
+module.exports = {
+  extends: ['imt', 'czx'],
+  rules:{
+    
+  }
+};
---
+++ src/const.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pluginName: 'RetryPlugin',
+  SCRIPT: 'script',
+  LINK: 'link',
+  META: 'meta',
+};
